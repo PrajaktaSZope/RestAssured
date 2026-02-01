45a55917b75021807762429005a45921cb4ec43f
PrajaktaSZope <prajaktaszope@gmail.com>
2026-02-01 23:12:38 +0530

    push the Rest Assured programs

--- src/test/java/Examples_Practice/Ternary_Grade_Calculator.java
+++ src/test/java/Examples_Practice/Ternary_Grade_Calculator.java
@@ -1,4 +1,11 @@
 package Examples_Practice;
 
 public class Ternary_Grade_Calculator {
+    public static void main(String[] args) {
+        String grade = args[0];
+        int marks = Integer.parseInt(grade);
+        String result = (marks<59) ? "F" : (marks<69) ? "D" : (marks<79) ? "C" :(marks<89) ? "B" :"A";
+        System.out.println(result);
+
+    }
 }
